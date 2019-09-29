eea35adfd98414291a57ca977d6699dd7bdd692b
iAlzar <srichardson97402@hotmail.com>
2019-09-29 16:57:25 -0700

    Add myId & myName to SetupData initial call

--- server/main.lua
+++ server/main.lua
@@ -10,5 +10,10 @@ AddEventHandler('mythic_base:server:CharacterSpawned', function()
     local char = exports['mythic_base']:FetchComponent('Fetch'):Source(src):GetData('character')
     local cData = char:GetData()
 
-    TriggerClientEvent('mythic_phone:client:SetupData', src, { { name = 'myNumber', data = cData.phone }, { name = 'apps', data = Config.Apps } })
+    TriggerClientEvent('mythic_phone:client:SetupData', src, {
+        { name = 'myId', data = cData.id },
+        { name = 'myName', data = cData.firstName .. ' ' .. cData.lastName },
+        { name = 'myNumber', data = cData.phone },
+        { name = 'apps', data = Config.Apps }
+    })
 end)
