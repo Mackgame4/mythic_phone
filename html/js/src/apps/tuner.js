a7a4f9cc71fc643b424fc4d46eaa3ac22f621c52
Alzar <srichardson97402@hotmail.com>
2019-10-07 16:39:27 -0700

    Update tuner.js

--- html/js/src/apps/tuner.js
+++ html/js/src/apps/tuner.js
@@ -2,6 +2,9 @@ import App from '../app';
 import Config from '../config';
 import Utils from '../utils';
 import Data from '../data';
+import anime from 'animejs/lib/anime.es.js';
+
+var timer = null;
 
 $('.tuner-nav').on('click', function(e) {
     if ($(this).data('disabled')) return;
@@ -19,10 +22,15 @@ function SetupTuner(tunerActive) {
         $('.no-chip-error').show('scale', function() {
             $('.tuner-nav').data('disabled', true);
         });
+    } else {
+        timer = setTimeout(function() {
+            $('#tuner-home-screen').fadeIn('normal');
+        }, 5000)
     }
 }
 
 function CloseApp() {
+    clearTimeout(timer);
     $('.no-chip-error').hide();
     $('.tuner-nav').removeData('disabled');
 }
